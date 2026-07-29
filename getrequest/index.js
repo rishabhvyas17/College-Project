10946a686c0c8ebedf940767e3d61f668595d91c
rishabhvyas17 <rishabhvyas222@gmail.com>
2026-07-29 10:29:00 +0530

    get request

--- getrequest/index.js
+++ getrequest/index.js
@@ -1,11 +1,37 @@
-import express from 'express'
+const express = require('express');
 const app = express()
-
 const port = 3030;
+
+const users = [
+    {
+        name:"Rishabh",
+        age:20
+    },
+    {
+        name:"Vyas",
+        age:20
+    },
+    {
+        name:"Prince",
+        age:20
+    }
+]
 
 app.get('/',(req,res)=>{
     res.send("Hello World!");
 });
+
+app.get('/about',(req,res)=>{
+    res.send("<h1>This is About Page</H1>");
+})
+
+app.get('/home',(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.get('/user',(req,res)=>{
+    res.json(users);
+})
 
 app.listen(port, ()=>{
     console.log(`Example app running on port ${port}`);
